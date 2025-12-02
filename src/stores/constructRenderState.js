068bb0d6638548476f15440284a959d3c68ba9e3
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-12-02 12:54:11 +0100

    Fix BGM audio properties (#60)

--- src/stores/constructRenderState.js
+++ src/stores/constructRenderState.js
@@ -466,9 +466,9 @@ export const addBgm = (
       id: "bgm",
       type: "sound",
       src: audioResource.fileId,
-      loop: audioResource.loop ?? true,
-      volume: audioResource.volume ?? 0.5,
-      delay: audioResource.delay ?? null,
+      loop: presentationState.bgm.loop ?? true,
+      volume: presentationState.bgm.volume ?? 0.5,
+      delay: presentationState.bgm.delay ?? null,
     });
   }
   return state;
