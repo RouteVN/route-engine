cd9427d2699e19fee1d486a0edc564ff99f5c767
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-12-26 11:01:41 +0800

    clean up code & setup ci (#83)

--- vt/static/main.js
+++ vt/static/main.js
@@ -176,72 +176,72 @@ const init = async () => {
       const uniqueEffects = Object.values(deduplicatedEffects);
 
       for (const effect of uniqueEffects) {
-      if (effect.name === 'render') {
-        const renderState = engine.selectRenderState();
-        routeGraphics.render(renderState);
-      } else if (effect.name === 'handleLineActions') {
-        engine.handleLineActions();
-      } else if (effect.name === 'startAutoNextTimer') {
-        // Remove old callback if exists
-        if (autoModeCallback) {
-          ticker.remove(autoModeCallback);
-        }
-
-        // Reset elapsed time
-        autoModeElapsed = 0;
-
-        // Create new ticker callback for auto mode
-        autoModeCallback = (time) => {
-          autoModeElapsed += time.deltaMS;
-
-          // Auto advance every 1000ms (1 second) - hardcoded
-          // TODO: Speed can adjust in the future
-          if (autoModeElapsed >= 1000) {
-            autoModeElapsed = 0;
-            engine.handleAction('nextLine', {});
+        if (effect.name === 'render') {
+          const renderState = engine.selectRenderState();
+          routeGraphics.render(renderState);
+        } else if (effect.name === 'handleLineActions') {
+          engine.handleLineActions();
+        } else if (effect.name === 'startAutoNextTimer') {
+          // Remove old callback if exists
+          if (autoModeCallback) {
+            ticker.remove(autoModeCallback);
           }
-        };
 
-        // Add to auto ticker
-        ticker.add(autoModeCallback);
-      } else if (effect.name === 'clearAutoNextTimer') {
-        // Remove ticker callback
-        if (autoModeCallback) {
-          ticker.remove(autoModeCallback);
-          autoModeCallback = null;
-        }
-        autoModeElapsed = 0;
-      } else if (effect.name === 'startSkipNextTimer') {
-        // Remove old callback if exists
-        if (skipModeCallback) {
-          ticker.remove(skipModeCallback);
-        }
+          // Reset elapsed time
+          autoModeElapsed = 0;
 
-        // Reset elapsed time
-        skipModeElapsed = 0;
+          // Create new ticker callback for auto mode
+          autoModeCallback = (time) => {
+            autoModeElapsed += time.deltaMS;
 
-        // Create new ticker callback for skip mode
-        skipModeCallback = (time) => {
-          skipModeElapsed += time.deltaMS;
+            // Auto advance every 1000ms (1 second) - hardcoded
+            // TODO: Speed can adjust in the future
+            if (autoModeElapsed >= 1000) {
+              autoModeElapsed = 0;
+              engine.handleAction('nextLine', {});
+            }
+          };
 
-          // Skip advance every 30ms
-          if (skipModeElapsed >= 30) {
-            skipModeElapsed = 0;
-            engine.handleAction('nextLine', {});
+          // Add to auto ticker
+          ticker.add(autoModeCallback);
+        } else if (effect.name === 'clearAutoNextTimer') {
+          // Remove ticker callback
+          if (autoModeCallback) {
+            ticker.remove(autoModeCallback);
+            autoModeCallback = null;
           }
-        };
+          autoModeElapsed = 0;
+        } else if (effect.name === 'startSkipNextTimer') {
+          // Remove old callback if exists
+          if (skipModeCallback) {
+            ticker.remove(skipModeCallback);
+          }
 
-        // Add to skip ticker
-        ticker.add(skipModeCallback);
-      } else if (effect.name === 'clearSkipNextTimer') {
-        // Remove ticker callback
-        if (skipModeCallback) {
-          ticker.remove(skipModeCallback);
-          skipModeCallback = null;
+          // Reset elapsed time
+          skipModeElapsed = 0;
+
+          // Create new ticker callback for skip mode
+          skipModeCallback = (time) => {
+            skipModeElapsed += time.deltaMS;
+
+            // Skip advance every 30ms
+            if (skipModeElapsed >= 30) {
+              skipModeElapsed = 0;
+              engine.handleAction('nextLine', {});
+            }
+          };
+
+          // Add to skip ticker
+          ticker.add(skipModeCallback);
+        } else if (effect.name === 'clearSkipNextTimer') {
+          // Remove ticker callback
+          if (skipModeCallback) {
+            ticker.remove(skipModeCallback);
+            skipModeCallback = null;
+          }
+          skipModeElapsed = 0;
         }
-        skipModeElapsed = 0;
       }
-    }
     };
   };
 
